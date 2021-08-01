b4c7375aa0daa7d5dfed14e919d016b9d13bd6fe
Jose Carlos <josecarlos@uimunicipalistas.org>
2021-08-01 14:07:07 +0200

    validaciones

--- src/main/java/josecarlos/pruebas/product/controller/ProductController.java
+++ src/main/java/josecarlos/pruebas/product/controller/ProductController.java
@@ -1,15 +1,24 @@
 package josecarlos.pruebas.product.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import josecarlos.pruebas.product.entity.Category;
 import josecarlos.pruebas.product.entity.Product;
 import josecarlos.pruebas.product.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -45,7 +54,10 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<Product> createProduct(@RequestBody Product product){
+    public ResponseEntity<Product> createProduct(@Valid @RequestBody Product product, BindingResult result){
+        if (result.hasErrors()){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,this.formatMessaje(result));
+        }
         return ResponseEntity.status(HttpStatus.CREATED).body(productService.createProduct(product));
     }
 
@@ -75,5 +87,26 @@ public class ProductController {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(product);
+    }
+
+    private String formatMessaje(BindingResult result){
+        List<Map<String,String>> errors = result.getFieldErrors().stream()
+                .map(err ->{
+                    Map<String,String> error = new HashMap<>();
+                    error.put(err.getField(),err.getDefaultMessage());
+                    return error;
+                }).collect(Collectors.toList());
+        ErrorMessage errorMessage = ErrorMessage.builder()
+                .code("01")
+                .messages(errors)
+                .build();
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString="";
+        try{
+            jsonString = mapper.writeValueAsString(errorMessage);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return jsonString;
     }
 }
--- src/main/java/josecarlos/pruebas/product/entity/Product.java
+++ src/main/java/josecarlos/pruebas/product/entity/Product.java
@@ -7,6 +7,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import java.util.Date;
 
 @Entity
@@ -17,14 +20,17 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty(message = "El nombre no puede estár vacío")
     private String name;
     private String description;
+    @Positive(message = "El stock debe ser mayor de 0")
     private Double stock;
     private Double price;
     private String status;
     @Column(name = "create_at")
     @Temporal(TemporalType.TIMESTAMP)
     private Date createdAt;
+    @NotNull(message = "La categoria no puede estar vacía")
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id")
     @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
